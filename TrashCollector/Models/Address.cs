8990c80a70c34044020c16d40945e5cb706c9eb2
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-02-18 09:51:59 -0800

    Wrote all necessary functions for Address model

--- TrashCollector/Models/Address.cs
+++ TrashCollector/Models/Address.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,12 @@ namespace TrashCollector.Models
 {
     public class Address
     {
-
+        [Key]
+        public int Id { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
 
     }
 }
