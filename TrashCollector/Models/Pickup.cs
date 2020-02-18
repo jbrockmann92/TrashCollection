8ceb2b80fc5be24b4e83e3a4bbf6293bf98d258a
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-02-18 09:58:07 -0800

    Added all necessary member variables for Pickup model

--- TrashCollector/Models/Pickup.cs
+++ TrashCollector/Models/Pickup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,13 @@ namespace TrashCollector.Models
 {
     public class Pickup
     {
-
+        [Key]
+        public int Id { get; set; }
+        public string PickupDay { get; set; }
+        public bool IsSuspended { get; set; }
+        public DateTime SuspendedStart { get; set; }
+        public DateTime SuspendedEnd { get; set; }
+        public DateTime OneTimePickup { get; set; }
 
     }
 }
