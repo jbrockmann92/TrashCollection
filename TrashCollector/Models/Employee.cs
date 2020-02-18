07ae1f6a6de576d6c4f0ac60ff8529c8d6a68a11
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-02-18 11:19:46 -0800

    Added necessary member variables for Customer Model

---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ZipCode { get; set; }
+        
+        [ForeignKey]
+        
+    }
+}
